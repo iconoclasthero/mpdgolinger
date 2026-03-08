609d89a67b2e4a524b21dad95b839a9ad4e0b3cc
iconoclast hero <iconoclasthero@gmail.com>
2026-03-07 19:29:24 -0500

    auto commit:

--- mpdgolinger.go
+++ mpdgolinger.go
@@ -374,6 +374,7 @@ func audioFromRaw(raw map[string]string, p string) AudioV1 {
   if year == "" {
     year = src
   }
+  log.Printf("raw[title]=%s", raw["title"])
   log.Printf("raw[duration]=%s", raw["duration"])
   duration, _ := strconv.ParseFloat(raw["duration"], 64)
 
