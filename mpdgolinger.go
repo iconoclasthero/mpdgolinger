783edb190a23d9aa480261bc63cfab419ca15d60
iconoclast hero <iconoclasthero@gmail.com>
2026-04-01 19:07:23 -0400

    auto commit:

--- mpdgolinger.go
+++ mpdgolinger.go
@@ -460,7 +460,10 @@ func refreshPulse() (PulseV1, error) {
   }
 
 log.Printf("[pulse] XDG_RUNTIME_DIR=%q UID=%d", os.Getenv("XDG_RUNTIME_DIR"), os.Getuid())
-  cmd := exec.Command("XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/pactl", "--format=json", "list", "sinks")
+  cmd := exec.Command("/usr/bin/pactl", "--format=json", "list", "sinks")
+cmd.Env = append(os.Environ(),
+  "XDG_RUNTIME_DIR=/run/user/1000",
+)
 //  out, err := cmd.Output()
 //  if err != nil {
 //    return PulseV1{}, err
